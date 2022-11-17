17c2c6b756626d3ff1cf029b3056e4b0f78c05f5
weiran1999 <907221539@qq.com>
2022-11-17 17:44:40 +0800

    feat: init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/App.css';
+import KeyInput from "./Input";
+
+export default function App() {
+    return (
+        <div className="selector-container">
+            <KeyInput />
+        </div>
+    );
+}
+
