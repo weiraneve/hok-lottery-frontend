9e41078adb1583a0934b86f4f5673d7688b809fd
weiran1999 <907221539@qq.com>
2022-11-22 12:43:42 +0800

    feat: add navigate-redirect to v6-router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './styles/App.css';
 import KeyInput from './components/Input';
-import {Route, Routes} from 'react-router-dom';
+import {Navigate, Route, Routes} from 'react-router-dom';
 
 export default function App() {
     return (
         <Routes>
-            <Route path='/' element={<KeyInput/>}/>
+            <Route path='/' element={<KeyInput/>} />
+
+            <Route path='*' element={<Navigate to='/' />} />
         </Routes>
     );
 }
